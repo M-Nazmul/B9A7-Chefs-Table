1776a006db02c3baf35092700108188bc447e7ab
M-Nazmul <najmulni839@gmail.com>
2024-03-18 03:40:13 +0600

    updated ChefsTable

--- src/components/ChefsTables/ChefsTable.jsx
+++ src/components/ChefsTables/ChefsTable.jsx
@@ -12,38 +12,61 @@ function ChefsTable() {
                     </div>
                 </div>
             </div> */}
-            <div className="overflow-x-auto">
-                <table className="table">
+            <div className="overflow-x-auto md:pr-3">
+                <h2 className="text-center text-2xl font-semibold pb-4">Want to cook: 01</h2> <hr />
+                <table className="table mt-4">
                     {/* head */}
                     <thead>
                     <tr>
                         <th></th>
                         <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
+                        <th>Time</th>
+                        <th>Calories</th>
                     </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
                             <tr className="bg-base-200">
                             <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
+                            <td>Chicken Caesar Salad</td>
+                            <td>30 minutes</td>
+                            <td>400 calories</td>
+                            <td><button className="btn bg-[#0be58a] hover:bg-transparent hover:border-[#0be58a] rounded-full px-6">Preparing</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h2 className="text-center text-2xl font-semibold pb-4 mt-4">Currently cooking: 02</h2> <hr />
+                <table className="table mt-4">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Calories</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                            <tr className="bg-base-200">
+                            <th>1</th>
+                            <td>Spaghetti Bolognese</td>
+                            <td>30 minutes</td>
+                            <td>400 calories</td>
                         </tr>
                         {/* row 2 */}
-                            <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
+                            <tr className="bg-base-200">
+                            <th>1</th>
+                            <td>Spaghetti Bolognese</td>
+                            <td>30 minutes</td>
+                            <td>400 calories</td>
                         </tr>
-                    {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
+                        {/* row 2 */}
+                            <tr className="">
+                            <th></th>
+                            <td></td>
+                            <td>Total Time = 45 minutes</td>
+                            <td>Total Calories = 1050 calories</td>
                         </tr>
                     </tbody>
                 </table>
